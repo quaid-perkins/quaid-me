f9deda076c84800d84ac735bd7b4eaae81c5bf6a
quaid-perkins <quaidperkins@gmail.com>
2025-10-21 12:34:50 +0100

    refactor: switch to '@/*' alias

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,14 @@
+import { resolve } from "node:path";
 import { defineConfig } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
+    },
   },
 });
