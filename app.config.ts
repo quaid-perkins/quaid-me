3ee02a1d8417404d06857d6f5e79b27526596a1e
quaid-perkins <quaidperkins@gmail.com>
2025-10-21 15:27:25 +0100

    fix: dirname for alias

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,10 @@
-import { resolve } from "node:path";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   vite: {
