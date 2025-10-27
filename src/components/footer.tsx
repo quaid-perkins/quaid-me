1f1abe6a47734d2bfa82816077cd7ff1781a71e3
quaid-perkins <quaidperkins@gmail.com>
2025-10-27 19:16:20 +0000

    feat: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,24 @@
+import { GithubIcon } from "lucide-solid";
 import type { Component } from "solid-js";
+import { Button } from "@/components/ui/button";
 
 const Footer: Component = () => {
-  return <footer class="mt-auto w-full p-4"></footer>;
+  return (
+    <footer class="mt-auto flex w-full justify-between p-4 text-xs text-zinc-700 sm:text-sm dark:text-zinc-300">
+      {"Quaid Perkins © All Rights Reserved"}
+      <div class="flex gap-2">
+        <Button
+          as="a"
+          href="https://github.com/quaid-perkins/quaid-me"
+          target="_blank"
+          variant="clear"
+          class="size-4"
+        >
+          <GithubIcon />
+        </Button>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
